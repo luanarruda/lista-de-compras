5bf352b9f22dd2c935738cdea85e3b632b4b08aa
luanarruda <luanamarrudaa@gmail.com>
2025-06-08 13:53:18 -0300

    refactor: Extract item creation function to a separate module

--- index.js
+++ index.js
@@ -1,67 +1,13 @@
+import { criarItemDaLista } from "./scripts/criarItemDaLista.js";
 const listaDeCompras = document.getElementById("lista-de-compras");
-const inputItem = document.getElementById("input-item")
-//diferenca de let e const = let pode ser alterado, const não
-//const é uma constante, ou seja, não pode ser alterada
+
 let contador = 0;
 
 //captar o valor a partir de uma determinada ação
 const botaoAdicionar = document.getElementById("adicionar-item");
 botaoAdicionar.addEventListener("click", (evento) => {
-
-	//evento padrao é recarregar a pagina
-	//por isso, vamos prevenir o evento padrão
-	evento.preventDefault();
-	if (inputItem.value === "") {
-		alert("Digite um item para adicionar");
-		return;
-	}
-
-	//criar um elemento li
-	const itemDaLista = document.createElement("li");
-	const containerItemDaLista = document.createElement("div");
-	//criar/puxar a classe do container
-	containerItemDaLista.classList.add("lista-item-container");
-	//checkbox é um atributo, tipo do input
-	const inputCheckbox = document.createElement("input");
-	inputCheckbox.type = "checkbox";
-	//adicionar o id dinamico
-	inputCheckbox.id = "checkbox-" + contador++;
-	//adicionar o paragrafo
-	const nomeItem = document.createElement("p");
-	//nessa variavel vem o q digitamos no input
-	nomeItem.innerText = inputItem.value;
-	//estilo dinamico
-	inputCheckbox.addEventListener("click", function () {
-		if (inputCheckbox.checked) {
-			nomeItem.style.textDecoration = "line-through";
-		}
-		else {
-			nomeItem.style.textDecoration = "none";
-		}
-	});
-
-	containerItemDaLista.appendChild(inputCheckbox);
-	containerItemDaLista.appendChild(nomeItem);
-	itemDaLista.appendChild(containerItemDaLista);
-
-	//adicionar o dia da semana
-	const diaDaSemana = new Date().toLocaleDateString("pt-BR", {
-		weekday: "long"
-	});
-	const data = new Date().toLocaleDateString("pt-BR");
-	const hora = new Date().toLocaleTimeString("pt-BR", {
-		hour: "numeric",
-		minute: "numeric"
-	});
-
-	const dataCompleta = `${diaDaSemana} (${data}) às ${hora}`;
-	const itemData = document.createElement("p");
-	itemData.innerText = dataCompleta;
-	itemData.classList.add("texto-data");
-
-	itemDaLista.appendChild(itemData);
+	const itemDaLista = criarItemDaLista();
 	listaDeCompras.appendChild(itemDaLista);
-
 	verificarListaVazia();
 
 })
@@ -75,6 +21,7 @@ function verificarListaVazia() {
 	if(itensDaLista.length === 0) {
 		//no momento ele coloca a msg para aparecer
 		mensagemListaVazia.style.display = "block"
+		mensagemListaVazia.style.backgroundColor = "#f8d7da";
 	} 
 	//remvoe a mensagem se a lista não estiver vazia
 	else{
---
+++ scripts/criarItemDaLista.js
@@ -0,0 +1,59 @@
+const inputItem = document.getElementById("input-item")
+
+export function criarItemDaLista() {
+
+	//evento padrao é recarregar a pagina
+	//por isso, vamos prevenir o evento padrão
+	evento.preventDefault();
+	if (inputItem.value === "") {
+		alert("Digite um item para adicionar");
+		return;
+	}
+
+	//criar um elemento li
+	const itemDaLista = document.createElement("li");
+	const containerItemDaLista = document.createElement("div");
+	//criar/puxar a classe do container
+	containerItemDaLista.classList.add("lista-item-container");
+	//checkbox é um atributo, tipo do input
+	const inputCheckbox = document.createElement("input");
+	inputCheckbox.type = "checkbox";
+	//adicionar o id dinamico
+	inputCheckbox.id = "checkbox-" + contador++;
+	//adicionar o paragrafo
+	const nomeItem = document.createElement("p");
+	//nessa variavel vem o q digitamos no input
+	nomeItem.innerText = inputItem.value;
+	//estilo dinamico
+	inputCheckbox.addEventListener("click", function () {
+		if (inputCheckbox.checked) {
+			nomeItem.style.textDecoration = "line-through";
+		}
+		else {
+			nomeItem.style.textDecoration = "none";
+		}
+	});
+
+	containerItemDaLista.appendChild(inputCheckbox);
+	containerItemDaLista.appendChild(nomeItem);
+	itemDaLista.appendChild(containerItemDaLista);
+
+	//adicionar o dia da semana
+	const diaDaSemana = new Date().toLocaleDateString("pt-BR", {
+		weekday: "long"
+	});
+	const data = new Date().toLocaleDateString("pt-BR");
+	const hora = new Date().toLocaleTimeString("pt-BR", {
+		hour: "numeric",
+		minute: "numeric"
+	});
+
+	const dataCompleta = `${diaDaSemana} (${data}) às ${hora}`;
+	const itemData = document.createElement("p");
+	itemData.innerText = dataCompleta;
+	itemData.classList.add("texto-data");
+	itemDaLista.appendChild(itemData);
+
+	return itemDaLista;
+
+}
